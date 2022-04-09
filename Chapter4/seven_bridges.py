fd606d17046c635ddf22822919490dc7a10101dd
Alexander Tometzki <alex.tometzki@icloud.com>
2022-04-09 18:58:08 +0200

    Add beginning of seven bridges problem

---
+++ Chapter4/seven_bridges.py
@@ -0,0 +1,19 @@
+from graph import Graph, V
+from edge import Edge
+from typing import  List
+
+Path = List[Edge] # type alias for path
+
+if __name__ == "__main__":
+    seven_bridges_graph: Graph[V] = Graph(["A", "B", "C", "D"])
+    seven_bridges_graph.add_edge_by_vertices("A", "B")
+    seven_bridges_graph.add_edge_by_vertices("A", "B")
+    seven_bridges_graph.add_edge_by_vertices("A", "C")
+    seven_bridges_graph.add_edge_by_vertices("A", "D")
+    seven_bridges_graph.add_edge_by_vertices("A", "D")
+    seven_bridges_graph.add_edge_by_vertices("B", "C")
+    seven_bridges_graph.add_edge_by_vertices("D", "C")
+    print(seven_bridges_graph)
+   
+
+    
